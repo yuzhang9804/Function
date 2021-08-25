ae14d758ac5aaccc78bb2b3c0f49a13058dc2798
张宇 <yuzhang9804@gmail.com>
2021-08-25 23:07:10 +0800

    feat: sort

---
+++ src/mergeSort.js
@@ -0,0 +1,37 @@
+/**
+ * 归并排序
+ * 时间复杂度 O(nlogn)
+ */
+
+/**
+ * @param {Array} left 
+ * @param {Array} right 
+ * @returns {Array}
+ */
+const merge = (left, right) => {
+  const arr = []
+
+  while(left.length, right.length) {
+    if (left[0] < right[0]) arr.push(left.shift())
+    else arr.push(right.shift())
+  }
+  
+  return [...arr, ...left, ...right]
+}
+
+/**
+ * 
+ * @param {Array} arr 
+ * @returns {Array}
+ */
+const mergeSort = arr => {
+  const len = arr.length
+
+  if (len < 2) return arr
+
+  const mid = Math.floor(len / 2)
+  const left = arr.slice(0, mid)
+  const right = arr.slice(mid)
+
+  return merge(mergeSort(left), mergeSort(right))
+}
---
+++ src/quickSort.js
@@ -0,0 +1,24 @@
+/**
+ * 快速排序
+ */
+
+/**
+ * 
+ * @param {Array} arr 
+ */
+const quickSort = arr => {
+  if (arr.length < 2) return arr
+
+  const pivotIndex = Math.round(arr.length / 2)
+  const pivot = arr.splice(pivotIndex, 1)[0]
+
+  const left = []
+  const right = []
+
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i] < pivot) left.push(arr[i])
+    else right.push(arr[i])
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)]
+}
